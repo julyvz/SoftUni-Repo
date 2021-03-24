377edfb3e624d11e3ca02cb6ddc29a828da7f808
julyvz <julianvz@yahoo.com>
2021-03-24 15:10:32 +0200

    Add project files.

---
+++ C# Fundamentals 01 2021/05BASIC SYNTAX/06ForeignLanguages/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _06ForeignLanguages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+
+            string language = "";
+
+            switch (country)
+            {
+                case "England":
+                case "USA":
+                    language = "English";
+                    break;
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    language = "Spanish";
+                    break;
+                default:
+                    language = "unknown";
+                    break;
+            }
+            Console.WriteLine(language);
+        }
+    }
+}
---
+++ C# Fundamentals 01 2021/05BASIC SYNTAX/11MultiplicationTable2_0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11MultiplicationTable2_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int start = int.Parse(Console.ReadLine());
+
+            if (start > 10)
+            {
+                Console.WriteLine($"{n} X {start} = {n * start}");
+            }
+            else
+            {
+                for (int i = start; i <= 10; i++)
+                {
+                    Console.WriteLine($"{n} X {i} = {n * i}");
+                }
+            }
+        }
+    }
+}
